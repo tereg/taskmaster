d24d7406889113bbe56683d5b84b5edc27890cc6
tereg <teregalla@gmail.com>
2017-04-02 11:13:38 -0700

    Add complete method to items controller, completed? method to item model, and complete patch to routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,6 +38,12 @@ class ItemsController < ApplicationController
     @item.destroy
     redirect_to root_path
   end 
+
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
+    redirect_to root_path
+  end 
   
   private
 
